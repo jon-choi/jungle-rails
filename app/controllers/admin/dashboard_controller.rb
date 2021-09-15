2ea0935b7ce4d45347de01b97a24c06ad1d8504c
Jonathan Choi <jonchoi11@gmail.com>
2021-09-15 22:39:17 +0000

    add display of product and category count. also add message for unauthorized user

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,10 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME_KEY'], password: ENV['ADMIN_PASSWORD_KEY']
 
+  def denied 
+    render plain: "Please leave immediately."
+  end
+
   def show
     @product_count = Product.count
     @category_count = Category.count 
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,6 +2,10 @@ class Admin::ProductsController < ApplicationController
 
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME_KEY'], password: ENV['ADMIN_PASSWORD_KEY']
 
+  def denied 
+    render plain: "Please leave immediately."
+  end
+
   def index
     @products = Product.order(id: :desc).all
   end
