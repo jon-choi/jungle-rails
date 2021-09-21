704edb806118611dedce10df2547c4a400313c78
Jonathan Choi <jonchoi11@gmail.com>
2021-09-21 22:20:18 +0000

    add spec test for product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 3,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "a user clicks a product and gets redirected to product details page" do
+    #ACT
+    visit root_path
+
+    find('a h4').click
+
+    #DEBUG
+    save_screenshot
+
+    #VERIFY = there should only be one article containing product details per product
+    expect(page).to have_css 'article.product-detail', count: 1
+
+  end
 end
