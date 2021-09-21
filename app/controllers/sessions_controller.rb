ed3e7889938cf9a9c255fda5a0e407f8fc7a8310
Jonathan Choi <jonchoi11@gmail.com>
2021-09-21 04:07:51 +0000

    refactor for login. now works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     
-    if user = User.authenticate_with_credentials(params[:email].strip, params[:password])
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def self.authenticate_with_credentials(email, password)
-    email = email.strip.downcase
-    user = User.find_by_email(email)
+    
+    user = self.find_by_email(email.strip)
 
     if user && user.authenticate(password)
 
-      return user.id
+      return user
     else 
-      return false
+      return nil
     end
   end
 
