9749f13ff8a9c309848a89507882734bd1103d89
Jonathan Choi <jonchoi11@gmail.com>
2021-09-21 02:19:46 +0000

    add rspec tests to verify password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    
+    it "must be created with a password" do
+      @user = User.new(first_name: "Dennis", last_name: "Reynolds", email: "god@mail.com", password: nil, password_confirmation: nil)
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "is not valid if passwords do not match" do
+      @user = User.new(first_name: "Dennis", last_name: "Reynolds", email: "god@mail.com", password: "123", password_confirmation: "1")
+      expect(@user). to be_invalid
+    end
+
+    it "must be created with a password that matched confirmation" do
+      @user = User.new(first_name: "Dennis", last_name: "Reynolds", email: "god@mail.com", password: "123", password_confirmation: "123")
+      expect(@user.password).to eq(@user.password_confirmation)
+    end
+
+  end
 end
